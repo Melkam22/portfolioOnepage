babbd6f0e12befb189468a55e2474126a9029003
Ashu <ashenshi@gmail.com>
2020-07-27 16:58:15 +0200

    portfolio first commit

---
+++ interact.js
@@ -0,0 +1,24 @@
+/* slide nav-bar function */
+let show = document.getElementById('nav-wrapper');
+
+function showMenu(){
+    show.style.right = '0';
+}
+
+/*  */
+function closeMenu(){
+    show.style.right = '-180px';
+}
+
+/* input field function */
+/* let clearInput = document.getElementsByClassName('input');
+let button = document.getElementsByClassName('btn');
+
+clearInput.addEventListener('click', clickFunction);
+
+function clickFunction(){
+    if(clearInput === ''){
+
+    }
+}
+ */
